8ecbfbd4647c1e42988d990013368910db641364
Taiyo Suzuki <taiysuzu199@ed.amdsb.ca>
2021-10-04 09:18:01 -0400

    Add project files.

---
+++ 2D-Game/Form1.cs
@@ -0,0 +1,24 @@
+﻿/* Taiyo Suzuki
+ * October 4 2021
+ * Basic 2D game
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2D_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
