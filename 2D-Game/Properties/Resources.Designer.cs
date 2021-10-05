d3392e17eab2401cc8de49c69a8d7eb1c2449f8d
Taiyo Suzuki <taiysuzu199@ed.amdsb.ca>
2021-10-05 10:53:51 -0400

    redundant resource removed

--- 2D-Game/Properties/Resources.Designer.cs
+++ 2D-Game/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace _2D_Game.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap dojo {
-            get {
-                object obj = ResourceManager.GetObject("dojo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap forest {
             get {
                 object obj = ResourceManager.GetObject("forest", resourceCulture);
